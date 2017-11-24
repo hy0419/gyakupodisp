4d432c2df0559381aa7047fc71ecf7eeb5c7cd12
yuto <yuto@hiragayuutonoMacBook-Pro.local>
2017-11-24 14:33:38 +0900

    a

---
+++ js.js
@@ -0,0 +1,33 @@
+var text = "こんばんは"
+var flag = 0;
+
+var si = setInterval(changecolor,1000);
+
+function changecolor(){
+    if(flag === 0){
+        c();
+        flag = 1;
+    }else{
+        d();
+        flag = 0;
+    }
+}
+function c() {
+    $('body').css(
+        "background-color","red"
+    )
+}
+
+function d() {
+    $('body').css(
+        "background-color","green"
+    )
+}
+function stopinterval() {
+    clearInterval(si)
+}
+function youkoso() {
+    var name = document.getElementById("a").value;
+   var san = name+"さん";
+    alert(san);
+}
